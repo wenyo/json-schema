10957c004138ab4dacf778df63438e1cde55a9ee
wen yo <wenda897436@gmail.com>
2021-06-14 22:39:53 +0800

    :construction: regular expression pratice.v3

--- js/RegularExpressions.js
+++ js/RegularExpressions.js
@@ -4,7 +4,7 @@ var re2 = new RegExp('ab+c');
 
 // pattern
 // "\"
-// \b : border
+// \b : word boundary
 const sBackslash1 = /\bter\b/;
 console.log('----sBackslash1----');
 console.log('1', sBackslash1.test('interest')); // false
@@ -92,3 +92,62 @@ console.log('----x{n,m}----');
 console.log('1', /a{1,3}/.exec('candy'));
 console.log('2', /a{1,3}/.exec('caaandy'));
 console.log('3', /a{1,3}/.exec('caaaaaaaandy'));
+
+// [xyz] : matches x or y or z
+console.log('----[xyz]----');
+console.log('1', /[a-d]/.exec('brisketab'));
+console.log('2', /[a-d]/.exec('city'));
+console.log('3', /[a-z]+/.exec('test.i.ng'));
+console.log('4', /[\w]+/.exec('test.i.ng'));
+
+// [\b] : 倒退字元
+// \b : word boundary
+console.log(`----\b----`);
+console.log('1', /\bm/.exec('moon'));
+console.log('2', /oo\b/.exec('moon'));
+console.log('3', /oon\b/.exec('moon'));
+
+// \B : not word boundary
+console.log(`----\B----`);
+console.log('1', /\B.../.exec('nanday'));
+
+// \d : equal [0-9]
+console.log(`----\d----`);
+console.log(/\d/.exec('B2 is the suite number'));
+
+// \D : equal [^0-9]
+console.log(`----\D----`);
+console.log(/\D/.exec('B2 is the suite number'));
+
+// \s : equal [ \f\n\r\t\v\u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]
+console.log(`----\s----`);
+console.log(/\s\w*/.exec('foo bar'));
+
+// \S : equal [^ \f\n\r\t\v\u00a0\u1680\u180e\u2000-\u200a\u2028\u2029\u202f\u205f\u3000\ufeff]
+console.log(`----\S----`);
+console.log(/\S\w*/.exec('foo bar'));
+
+// \w : equal [A-Za-z0-9_]
+console.log(`----\w----`);
+console.log(/\w/.exec('foo bar'));
+console.log(/\w/.exec('$5.88'));
+
+// \W : equal [A-Za-z0-9_]
+console.log(`----\W----`);
+console.log(/\W/.exec('foo bar'));
+console.log(/\W/.exec('$5.88'));
+
+// \n : n is positive int
+console.log('----\n----');
+console.log('1', /apple(,)\sorange\1/.exec('apple, orange, cherry, peach.'));
+
+// \xhh : 16進制
+// \ddd : 3個 8進制
+
+// \c : U+000D
+// \r : U+000D
+// \f : U+000C
+// \n : U+000A
+// \t : U+0009 tab
+// \v : U+000B vertical tab
+// \0 : U+0000 NULL
